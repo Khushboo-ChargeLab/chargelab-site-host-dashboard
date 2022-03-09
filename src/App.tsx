18b25fd7ef90e44a9bce7ee12514f7c946d074f6
binchen0802 <100237925+binchen0802@users.noreply.github.com>
2022-03-09 08:59:37 -0700

    PM-320 remove csv and pdf exporter. change file extension to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
-import { useExport } from './hooks'
 
-const data = [
-  ["firstname", "lastname", "email"],
-  ["Ahmed", "Tomi", "ah@smthing.co.com"],
-  ["Raed", "Labes", "rl@smthing.co.com"],
-  ["Yezzi", "Min l3b", "ymin@cocococo.com"]
-];
 
 function App() {
   const { t, i18n } = useTranslation();
   const changeLanguage = (lng:string) => {
     i18n.changeLanguage(lng);
   };
-  const { setExport } = useExport();
+
   const a = 12389.12
   return (
     <div className="App">
@@ -33,10 +26,6 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       {a.toLocaleCurrency('EUR')}
       </h1>
-      <br/><br/>
-      <button onClick={() => setExport(data,'csv','testcsv')}>{t('export_csv')}</button>
-      <br/>
-      <button onClick={() => setExport(data,'pdf','testpdf')}>{t('export_pdf')}</button>
       <Route path="/login">
         <div>
         
--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-// Check custom hooks here:
-// https://chargelab.atlassian.net/wiki/spaces/PE/pages/1530953734/Custom+Hooks
-
-import useExport from './use-export'
-
-export {
-    useExport
-}
