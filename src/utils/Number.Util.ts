0d1c40f4c335ac9ba7953a93fea39fff8c8157b1
mohsen10 <mohsen@sidedooraccess.com>
2022-03-10 14:13:58 -0500

    check to see if input is valid

--- src/utils/Number.Util.ts
+++ src/utils/Number.Util.ts
@@ -1,3 +1,7 @@
-export const convertToThousandSeperator = (input:number) => {
-    return Intl.NumberFormat().format(input)
+export const convertToThousandSeperator = (input: number) => {
+  if (!input) {
+    return '';
   }
+  
+  return Intl.NumberFormat().format(input)
+}
