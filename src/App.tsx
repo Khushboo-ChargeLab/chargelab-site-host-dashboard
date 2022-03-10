65bfadaf0e030579c043d99c4337bc00fab5f5c5
mohsen10 <mohsen@sidedooraccess.com>
2022-03-10 14:04:04 -0500

    app clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { Route } from "react-router-dom";
-import { useTranslation } from 'react-i18next';
-import { convertToLocaleCurrency } from './utils/Currency.Util';
-import { formatDate,formatTime } from './utils/Date.Util';
 
 function App() {
-  const { t, i18n } = useTranslation();
-  const changeLanguage = (lng:string) => {
-    i18n.changeLanguage(lng);
-  };
-
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      {t('hello_world')}
-      </h1>
-      <button onClick={() => changeLanguage("en-US")}>English</button>
-      <button onClick={() => changeLanguage("es")}>Español</button>
-      <button onClick={() => changeLanguage("fr")}>Français</button>
-      <br/><br/>
-      <h1 className="text-3xl font-bold underline">
-      {convertToLocaleCurrency(12389.12,'EUR')}
-      <br/>{formatDate(new Date())}
-      <br/>{formatTime(new Date())}
-      </h1>
+     
       <Route path="/login">
         <div>
         
