4887c87648361f30fd39a4897bced5f1236e4378
mohsen10 <mohsen@sidedooraccess.com>
2022-03-23 14:03:12 -0400

    linting fixed

--- src/stores/selectors/app-navigation.selector.ts
+++ src/stores/selectors/app-navigation.selector.ts
@@ -1,11 +1,10 @@
-
 import { createSelector } from 'reselect';
 import { AppState } from '../types/App-State';
 
 const AppNavigationSelectors = (state: any) => state.navigator as AppState;
 const initStore = {
-    title:'Overview',
-    path:'/overview',
+    title: 'Overview',
+    path: '/overview',
 };
 
 export const getCurrentNavigation = createSelector(
--- src/utils/Date.Util.ts
+++ src/utils/Date.Util.ts
@@ -1,17 +1,17 @@
 import { format, isValid } from 'date-fns';
 
-export const formatDate = (date: Date, dateFormat: string = "LLL dd, yyyy") => {
+export const formatDate = (date: Date, dateFormat: string = 'LLL dd, yyyy') => {
     if (isValid(date)) {
         return format(date, dateFormat || 'LLL dd, yyyy');
     }
 
     return '';
-}
+};
 
-export const formatTime = (date: Date, dateFormat: string = "HH:mm") => {
+export const formatTime = (date: Date, dateFormat: string = 'HH:mm') => {
     if (isValid(date)) {
         return format(date, dateFormat);
     }
 
     return '';
-}
+};
