83b7d41667c68b623b6bb8c2019b91c7fe4e2f46
mohsen10 <mohsen@sidedooraccess.com>
2022-04-28 14:48:25 -0400

    charger status chart

--- src/components/overview/Charger-Status-Chart.component.tsx
+++ src/components/overview/Charger-Status-Chart.component.tsx
@@ -1,23 +1,75 @@
-import { memo } from 'react';
-import { Card, DoughnutChart, Label, LabelType, Button, ButtonType } from '../_ui';
+import { memo, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectChargers } from '../../stores/selectors/charger.selector';
+import {
+  Card,
+  DoughnutChart,
+  Label,
+  LabelType,
+  Button,
+  ButtonType,
+} from '../_ui';
 import { ButtonSize } from '../_ui/Button.component';
 import { ChartData } from '../_ui/chart/types/Chart-Data.interface';
 
 interface InputProps {
-  data?:ChartData[];
+  data?: ChartData[];
 }
-export const ChargerStatusChart = memo(({
-  data = [],
-}: InputProps) => (
-  <Card className='h-full'>
-    <div className="flex w-full mb-4">
-      <div className="flex w-1/2">
-        <Label type={LabelType.H4} text="Charger Status" className='pb-6' />
+export const ChargerStatusChart = memo(({ data = [] }: InputProps) => {
+  const [chargerStatus, setChargerStatus] = useState<any>([]);
+  const chargers = useSelector(selectChargers);
+
+  useEffect(() => {
+    if (chargers && chargers.length) {
+      setChargerStatus([
+        {
+          label: 'Available',
+          value: chargers.filter(
+            (c) =>
+              c.status === 'ONLINE' &&
+              c.ports.some((d) => d.status === 'AVAILABLE'),
+          ).length,
+          color: '#7CB342',
+        },
+        {
+          label: 'Charging',
+          value: chargers.filter(
+            (c) =>
+              c.status === 'ONLINE' &&
+              (c.ports.every((d) => d.status === 'IDLE') ||
+                c.ports.every((d) => d.status === 'CHARGING')),
+          ).length,
+          color: '#039BE5',
+        },
+        {
+          label: 'Offline',
+          value: chargers.filter((c) => c.status === 'OFFLINE').length,
+          color: '#FFB300',
+        },
+        {
+          label: 'Coming soon',
+          value: chargers.filter((c) => c.status === 'COMING_SOON').length,
+          color: '#B0B8C1',
+        },
+      ]);
+    }
+  }, [chargers]);
+
+  return (
+    <Card className='h-full'>
+      <div className='flex w-full mb-4'>
+        <div className='flex w-1/2'>
+          <Label type={LabelType.H4} text='Charger Status' className='pb-6' />
+        </div>
+        <div className='flex justify-end w-1/2'>
+          <Button
+            size={ButtonSize.SMALL}
+            label='View chargers'
+            type={ButtonType.Cancel}
+          />
+        </div>
       </div>
-      <div className="flex justify-end w-1/2">
-        <Button size={ButtonSize.SMALL} label="View chargers" type={ButtonType.Cancel} />
-      </div>
-    </div>
-    <DoughnutChart items={data} className='flex h-32' />
-  </Card>
-    ));
+      <DoughnutChart items={chargerStatus} className='flex h-32' />
+    </Card>
+  );
+});
--- src/components/overview/Overview.component.tsx
+++ src/components/overview/Overview.component.tsx
@@ -14,29 +14,6 @@ export const Overview = () => {
   const dispatch = useDispatch();
   const locations = useSelector(getLocation);
 
-  const chargerStatus = [
-    {
-      label: 'Available',
-      value: 4,
-      color: '#7CB342',
-    },
-    {
-      label: 'Charging',
-      value: 1,
-      color: '#039BE5',
-    },
-    {
-      label: 'Offline',
-      value: 3,
-      color: '#FFB300',
-    },
-    {
-      label: 'Coming soon',
-      value: 2,
-      color: '#B0B8C1',
-    },
-  ];
-
   const locationChanged = (location: any) => {
     dispatch(fetchSessions({ locations: location }));
     dispatch(fetchSimpleStat({ locations: location }));
@@ -60,7 +37,7 @@ export const Overview = () => {
       </div>
       <div className='flex w-full mt-6'>
         <div className='inline-block w-2/5 pr-3'>
-          <ChargerStatusChart data={chargerStatus} />
+          <ChargerStatusChart />
         </div>
         <div className='inline-block w-3/5 pl-3'>
           <Summary />
