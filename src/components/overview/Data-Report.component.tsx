612d743d440d4b3c4cc36aac8e8dc4f25eb9bfee
binchen0802 <100237925+binchen0802@users.noreply.github.com>
2022-05-06 14:49:51 -0600

    BF-354: format tick and tooltip label

--- src/components/overview/Data-Report.component.tsx
+++ src/components/overview/Data-Report.component.tsx
@@ -5,6 +5,7 @@ import { selectSimpleStats } from '../../stores/selectors/session.selector';
 import { Button, Card, DateTimePicker, Switch, VerticalBarChart } from '../_ui';
 import { ButtonSize, ButtonType } from '../_ui/Button.component';
 import './Data-Report.component.scss';
+import { convertToLocaleCurrency } from '../../utils/Currency.Util';
 
 export const DataReport = () => {
   const dispatch = useDispatch();
@@ -41,6 +42,24 @@ export const DataReport = () => {
     dispatch(fetchSimpleStat(null));
   }, [dispatch]);
 
+  const getFormattedText = (val: any) => {
+    switch (valueField) {
+      case 'revenue': {
+        return convertToLocaleCurrency(val);
+      }
+      case 'energyDeliveredKWh': {
+        return `${val} kWh`;
+      }
+      default:
+        return val;
+    }
+  };
+  const onTooltipTitle = (value: any) => {
+    return getFormattedText(value);
+  };
+
+  const onTickLabel = (tickValue: any) => getFormattedText(tickValue);
+
   return (
     <Card>
       <div className='flex mt-3 mb-8 w-full'>
@@ -75,6 +94,8 @@ export const DataReport = () => {
         className='flex h-52 w-full'
         dateField='date'
         valueField={valueField}
+        onTickLabel={onTickLabel}
+        onTooltipTitle={onTooltipTitle}
       />
     </Card>
   );
