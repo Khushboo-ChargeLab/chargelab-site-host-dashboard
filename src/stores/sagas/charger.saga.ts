3f638b24da0c2299d461a5c1ca92e93398fc61a5
Khushboo-ChargeLab <you@example.com>
2022-05-18 12:51:36 -0400

    Uncommenting the flow for fetchChargerDetail API

--- src/stores/sagas/charger.saga.ts
+++ src/stores/sagas/charger.saga.ts
@@ -1,6 +1,6 @@
 import { put, takeEvery, call } from 'redux-saga/effects';
-import { getChargers, getChargerDetail } from '../../services/overview/charger.service';
-import { fetchChargerDetail, fetchChargerDetailSuccess, fetchChargers, fetchTroubleChargers, fetchChargersSuccess, fetchTroubleChargersSuccess } from '../reducers/charger.reducer';
+import { getChargers, getChargerDetail, updateCharger } from '../../services/overview/charger.service';
+import { fetchChargerDetail, fetchChargerDetailSuccess, fetchChargers, fetchTroubleChargers, fetchChargersSuccess, fetchTroubleChargersSuccess, updateChargerInformation, updateChargerInformationSuccess } from '../reducers/charger.reducer';
 
 function* watchFetchAllChargers(action:any): any {
     const response = yield call(getChargers, action.payload);
@@ -21,7 +21,7 @@ function* chargersSaga() {
     yield takeEvery(fetchChargers, watchFetchAllChargers);
     yield takeEvery(fetchTroubleChargers, watchFetchTroubleChargers);
     // TODO: CB use BE data from API
-    // yield takeEvery(fetchChargerDetail, watchFetchChargerDetail);
+    yield takeEvery(fetchChargerDetail, watchFetchChargerDetail);
 }
 
 export default chargersSaga;
